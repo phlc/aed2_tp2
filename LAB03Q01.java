18becd21d8c83d3b62f3bc4defd3c0ffe58880f7
phlc <pedrocarvalho@outlook.com>
2020-02-17 16:01:33 -0300

    LAB03Q01.java primeiro esboço

---
+++ LAB03Q01.java
@@ -0,0 +1,54 @@
+/**
+* LAB 3 - Questao 1  - Sequencia Espelho
+* @author Pedro Henrique Lima Carvalho
+* Matricula: 651230
+* AED2 - Tarde - Puc Minas
+*/
+public class LAB03Q01{
+	
+	/**
+	* Metodo main
+	*/
+	public static void main (String[] args){
+		String s = MyIO.readString();	
+
+		while (!isEnd(s)){
+			(espelho(s);
+			s = MyIO.readString();
+		}
+	}
+	
+
+	/**
+	* isEnd - Verifica o fim das entradas
+	* @param String
+	* @return boolean
+	*/
+	public static boolean isEnd (String s){
+		return (s.length()==3 && s.charAt(0)=='F' &&
+			s.charAt(1)=='I' && s.charAt(2)=='M');
+	}
+
+	/**
+	*espelho - mostra a sequencia numerica do intervelo de dois numeros espelhada 
+	*@param String
+	*/
+	public static void espelho (String s){
+		String resp = "";
+		String[] nums = s.split(" ", 2);
+		espelho(nums[0], nums[1]);
+	}
+
+	/**
+	*espelho - overload
+	*@param int int 
+	*/
+	public static void espelho (int ini, int fim){
+		if (ini <= fim){
+			MyIO.print(ini);
+			espelho(ini+1, fim);
+			MyIO.print(ini);
+		}
+	}
+}
+
